b17b201546c15d1c86d43cecaad7a30ec1171564
karin valcanover <valcanoverkarin@outlook.it>
2021-05-18 16:17:57 +0200

    inizio Snack 2

--- index.php
+++ index.php
@@ -33,6 +33,18 @@
     ];
 
     //var_dump($matches);
+
+
+    // Snack 2
+    // Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
+    // che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
+    // Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+    $name = $_GET["name"];
+    $mail = $_GET["mail"];
+    $age = $_GET["age"];
+
+
 ?>
 
 <!DOCTYPE html>
